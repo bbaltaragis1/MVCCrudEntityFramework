ff4ec774673d1a40d5027cbfdcc8cff6847ccfc5
bbaltaragis1 <dudeamialive@gmail.com>
2019-05-27 04:00:09 -0500

    Initial commit

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCCrudEntityFramework.Models;
+
+namespace MVCCrudEntityFramework.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer/Index
+        public ActionResult Index()
+        {
+            using (DBModels db = new DBModels())
+            {
+                return View(db.Customers.ToList());
+            }
+                
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            using (DBModels db = new DBModels())
+            {
+                return View(db.Customers.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            try
+            {
+                using (DBModels db = new DBModels())
+                {
+                    db.Customers.Add(customer);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DBModels db = new DBModels())
+            {
+                return View(db.Customers.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Customer customer)
+        {
+            try
+            {
+                using (DBModels db = new DBModels()) {
+                    db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    
+                }
+                return RedirectToAction("Index");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DBModels db = new DBModels())
+            {
+                return View(db.Customers.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Customer customer)
+        {
+            try
+            {
+                using (DBModels db = new DBModels())
+                {
+                    customer = db.Customers.Where(x => x.Id == id).FirstOrDefault();
+                    db.Customers.Remove(customer);
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
